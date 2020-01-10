c1e66517cfe0f880415b07f0468b83500d5d1849
Anil <anilkumarreddy778@gamil.com>
2020-01-10 19:24:10 +0530

    reverse integer

--- src/intrv_test/V3_Revers_a_integer.java
+++ src/intrv_test/V3_Revers_a_integer.java
@@ -3,24 +3,39 @@ package intrv_test;
 public class V3_Revers_a_integer {
 	
 	
+	public static void ReverseanInteger() {
+  // TODO Auto-generated method stub
+		  int k = 100;
+		  int r;
+		  while(k>0) {
+		   r=k%10;
+		   k=k/10;
+		   System.out.print(r);
+		   
+		  } 
+		  System.out.println();
+		 }
+
+		
+	
+	
 	public static void main(String[] args)
 	{
-		
-		long num=123453453;
+		ReverseanInteger();
+		long num=1001;
 		long rev=0;
 		
 		while(num !=0) 
 		{
-			rev=rev*10+num%10;
+			long ff=num%10;
+			rev=rev*10+num%10; // % -mod
 			num=num/10;
 		}
-		
 		
 		System.out.println(rev);
 		long num1=4537479;
 		System.out.println(new StringBuffer(String.valueOf(num1)).reverse());
-		
-		
+			
 	}
 
 }
