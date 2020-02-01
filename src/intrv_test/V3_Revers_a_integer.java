92015d13739fb3ac0dee20ebab0e47f6a50cbb4e
Anil <anilkumarreddy778@gamil.com>
2020-02-01 19:32:30 +0530

    minor changes url

--- src/intrv_test/V3_Revers_a_integer.java
+++ src/intrv_test/V3_Revers_a_integer.java
@@ -11,11 +11,20 @@ public class V3_Revers_a_integer {
 		   r=k%10;
 		   k=k/10;
 		   System.out.print(r); 
-		  } 
+		  }
 		  System.out.println();
 		 }
 
-		
+		public static void rever(int num) {
+			
+			String numdd=Integer.toString(num);
+			
+			for(int i=numdd.length()-1;i>=0;i--)
+			{
+				System.out.print(numdd.charAt(i));
+			}
+			System.out.println("");
+		}
 	
 	
 	public static void main(String[] args)
@@ -23,12 +32,11 @@ public class V3_Revers_a_integer {
 		//1st type
 		ReverseanInteger(420);
 		
-		
+		rever(4565);
 		//2nd type
 		long num=1201;
 		long rev=0;
-		
-		while(num !=0) 
+		while(num !=0)
 		{
 			long ff=num%10;
 			rev=rev*10+num%10; // % -mod
@@ -37,9 +45,10 @@ public class V3_Revers_a_integer {
 		System.out.println(rev);
 		
 		//3rd type
+		int nudm=465;
 		long num1=4537479;
 		System.out.println(new StringBuffer(String.valueOf(num1)).reverse());
-			
+		System.out.println(new StringBuffer(Integer.valueOf(nudm)).reverse());
 	}
 
 }
--- src/test/testPrime.java
+++ src/test/testPrime.java
@@ -4,61 +4,71 @@ import java.sql.Array;
 
 public class testPrime {
 	
-	public static boolean primenum(int num) {
-		System.out.print(num+ " is given number : ");
-		if(num<=1)
-		{return false;}
-		for(int i=2;i<num;i++)
-		{
-			if(num%i==0) {return false;} 
-				}
-		return true;
+public static void armstrong(int num)
+{
+	int cube=0;
+	int r=0;
+	int t;
+	t=num;
+	
+	while(num>0)
+	{
+		
+		r=num%10;
+		cube=cube+(r*r*r);
+		num=num/10;
 	}
 	
-	public static void polindrom(int num) {
-		int r=0;
-		int sum=0;
-		int t;
-		t=num;
-		while(num>0)
-		{
-			r=num%10;
-			sum=(sum*10)+r;
-			num=num/10;
-		}
-		if(sum==t) {System.out.println("it's polindrom");}else {System.out.println("it's not polindrom");}
+	if(cube==t) {System.out.println("it's armstrong");}else {System.out.println("it's not armstrong");}
+}
+
+public static void polindrom(int num) {
+	int r=0;
+	int sum=0;
+	int t;
+	t=num;
+	while(num>0) {
+		r=num%10;
+		sum=(sum*10)+r;
+		num=num/10;
 	}
 	
-	public static void armstrong(int num) {
-		int cube=0;
-		int r=0;
-		int t;
-		t=num;
-		while(num>0)
+	if(sum==t) {System.out.println("it's polindrom");}else {System.out.println("it's not polindrom");}
+}
+public static boolean primenum(int num) {
+	if(num<=1)
+	{return false;}
+	
+	for(int i=2;i<num;i++)
+	{
+		if(num%i==0)
 		{
-			r=num%10;
-			cube=cube+(r*r*r);
-			num=num/10;
+			return false;
 		}
-		if(cube==t) {System.out.println("it's armstrong");}else {System.out.println("it's not armstrong");}
 	}
-	public static void factroial(int num) {int fact=1;
+	return  true;
+}
+
+public static void factroial(int num)
+{
+	int fact=1;
+	
 	for(int i=1;i<=num;i++)
-		{
+	{
 		fact=fact*i;
-		}
-	System.out.println(fact);
 	}
+	System.out.println(fact);
+}
 	public static void main(String[] args)
 	{
 		
-		int num =20;
-		for(int i=0;i<=num;i++)
-		{
-		System.out.println(primenum(i));
-		}
+//		int num =20;
+//		for(int i=0;i<=num;i++)
+//		{
+		System.out.println(primenum(7));
+//		}
 		polindrom(123321);
-		armstrong(407);
+		armstrong(153);//153 , 407, 370 , 371 -  //1*1*1=1 //5*5*5=125 //3*3*3=27 //1+125+27=153
 		factroial(4);
 		
 		String test[];
@@ -68,10 +78,12 @@ public class testPrime {
 		test[2]="ram";
 		test[3]="sree";
 		test[4]="rahul";
+		test[5]="monk";
+		test[6]="test";
 		int test2[]=new int[10];
 		test2[0]=10;
 		
-
+		
 		// Array st=new Array();
 		
 		//declaration
@@ -83,13 +95,13 @@ public class testPrime {
 		arr[1]=20;
 		arr[2]=50;
 		arr[3]=30;
-		//arr[0]=340;
 		
 		
-		for(int i=0;i<arr.length;i++)
-		{
-			System.out.println(arr[i]+" : "+test[i]);
-		}
+//		
+//		for(int i=0;i<arr.length;i++)
+//		{
+//			System.out.println(arr[i]+" : "+test[i]);
+//		}
 		//System.out.println("120 is prime numb ::"+prime(2));
 //		int num=100;
 //		for(int i =0;i<=num;i++)
